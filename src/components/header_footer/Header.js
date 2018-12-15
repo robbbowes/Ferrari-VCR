573ec436211208f5637ccccae4239f7ae0e2ddad
Robb Bowes <up666724@myport.ac.uk>
2018-12-15 09:21:11 +0100

    Header finished, transparency on scroll functionality working

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -5,14 +5,44 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
+
 class Header extends Component {
+
+    state = {
+        drawerOpen: false,
+        headerTransparent: true
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        if (window.scrollY > 0) {
+            this.setState({
+                headerTransparent: false
+            })
+        } else {
+            this.setState({
+                headerTransparent: true
+            })
+        }
+    }
+
+    toggleDrawer = (value) => {
+        this.setState({
+            drawerOpen: value
+        })
+    }
+
     render() {
         return (
             <AppBar
                 position='fixed'
                 style={{
-                    backgroundColor: '#2f2f2f',
-                    boxShadow:'none',
+                    backgroundColor: this.state.headerTransparent ? 'transparent' : '#2f2f2f',
+                    boxShadow: 'none',
                     padding: '10px 0px'
 
                 }}
@@ -27,10 +57,15 @@ class Header extends Component {
                     <IconButton
                         arial-label="Menu"
                         color="inherit"
-                        onClick={() => console.log('Clicked')}
+                        onClick={() => this.toggleDrawer(true)}
                     >
-                        <MenuIcon/>
+                        <MenuIcon />
                     </IconButton>
+
+                    <SideDrawer
+                        open={this.state.drawerOpen}
+                        onClose={(value) => this.toggleDrawer(value)}
+                    />
 
                 </Toolbar>
             </AppBar>
